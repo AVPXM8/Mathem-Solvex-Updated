545647389659af8e571ffd60eccde7b031406b9c
AVPXM8 <113023480+AVPXM8@users.noreply.github.com>
2025-07-24 16:33:10 +0530

    Added correct api route

--- admin/src/api.js
+++ admin/src/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 // This creates a central instance of axios for your entire app.
 // By setting the baseURL to '/api', all requests will automatically go
 const api = axios.create({
-    baseURL: '/api' 
+  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:3001'
 });
 
 // This "interceptor" automatically adds your login token to every secure request.
--- student/src/api.js
+++ student/src/api.js
@@ -1,4 +1,3 @@
-// src/api.js - FINAL, SIMPLIFIED VERSION
 
 import axios from 'axios';
 
@@ -6,7 +5,7 @@ import axios from 'axios';
 // By setting the baseURL to '/api', all requests will automatically go
 // to the correct domain, whether on localhost or on your live Render site.
 const api = axios.create({
-    baseURL: '/api' 
+  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:3001'
 });
 
 // This "interceptor" automatically adds your login token to every secure request.
@@ -25,26 +24,4 @@ api.interceptors.request.use(
 );
 
 export default api;
-// import axios from 'axios';
-
-// // This is the dynamic base URL for all API calls.
-// const API_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:3001';
-
-// const api = axios.create({
-//     baseURL: `${API_URL}/api`,
-// });
-
-// // This automatically attaches the admin token to secure requests
-// api.interceptors.request.use(
-//     (config) => {
-//         const token = localStorage.getItem('adminToken');
-//         if (token) {
-//             config.headers['Authorization'] = `Bearer ${token}`;
-//         }
-//         return config;
-//     },
-//     (error) => Promise.reject(error)
-// );
-
-// export default api;
 
